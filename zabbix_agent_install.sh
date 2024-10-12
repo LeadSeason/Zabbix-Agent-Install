1f04b84c8763616964658435c9750c5506bf0442
LeadSeason <leadseason@pm.me>
2024-10-12 23:12:36 +0300

    Update zabbix_agent_install.sh

--- zabbix_agent_install.sh
+++ zabbix_agent_install.sh
@@ -1,13 +1,55 @@
-#!/bin/env bash
+#!/bin/bash
 
 set -e
 
+# Check root permissions
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Insufficient permissions."
+    exit 10
+fi
+
+# Default values
 ZABBIX_SERVER="10.10.10.10"
 ZABBIX_PORT="10050"
 ZABBIX_PKS_ENABLED=1
 
+usage() {
+    printf "%s\n" "Usage: $0 
+    --server <zabbix_server>    Server IP, seporated with \", \" for multi ip. Defaults to 10.10.10.10
+    --port <zabbix_port>        Zabbix-agent listening port. Defaults to 10050
+    --psk-enabled <0|1>         Enable psk, outputs psk settings at the end of the script. Defaults to 1.
+"
+    exit 1
+}
+
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        --server)
+            ZABBIX_SERVER="$2"
+            shift 2
+            ;;
+        --port)
+            ZABBIX_PORT="$2"
+            shift 2
+            ;;
+        --psk-enabled)
+            ZABBIX_PKS_ENABLED="$2"
+            shift 2
+            ;;
+        *)
+            echo "Unknown argument: $1"
+            usage
+            ;;
+    esac
+done
+
+echo "Zabbix Server: $ZABBIX_SERVER"
+echo "Zabbix Port: $ZABBIX_PORT"
+echo "PSK Enabled: $ZABBIX_PKS_ENABLED"
+
 SYSTEM_DISTRO=$(awk -F= '$1 == "ID" {print $2}' /etc/os-release)
-SYSTEM_IP=$(ip -o route get to 8.8.8.8 | sed -n 's/.*src \([0-9.]\+\).*/\1/p')
+SYSTEM_IP=$(ip -o route get to 8.8.8.8 | sed -n 's/.*src \([^ ]*\).*/\1/p')
+SYSTEM_IPV6=$(ip -o route get to 2001:4860:4860::8888 | sed -n 's/.*src \([^ ]*\).*/\1/p')
 SYSTEM_HOSTNAME=$(cat /etc/hostname)
 
 ZABBIX_CONF_LOCATION="/etc/zabbix/zabbix_agentd.conf"
@@ -16,16 +58,7 @@ ZABBIX_LOG_LOCATION="/var/log/zabbix/zabbix_agentd.log"
 ZABBIX_LOG_DIR_LOCATION="/var/log/zabbix/"
 ZABBIX_PID_LOCATION="/run/zabbix/zabbix_agentd.pid"
 ZABBIX_USER_NAME="zabbix"
-
-if [ "$(id -u)" -ne 0 ]; then
-    echo "Insufficient permissions."
-    exit 10
-fi
-
-echo "Zabbix server address: $ZABBIX_SERVER"
-
 # Install Zabbix-agent
-
 # Ubuntu
 if [ "$SYSTEM_DISTRO" = "Ubuntu" ]; then
 	echo "Ubuntu detected, continuing with install."
@@ -42,7 +75,7 @@ if [ "$SYSTEM_DISTRO" = "Ubuntu" ]; then
 	apt-get install zabbix-agent -y || exit 11
 
 # Debian
-elif [ "$SYSTEM_DISTRO" = "Debian" ]; then
+elif [ "$SYSTEM_DISTRO" = "debian" ]; then
 	echo "Debian detected, continuing with install."
 
 	apt-get update -y > /dev/null
@@ -110,16 +143,16 @@ systemctl enable zabbix-agent
 # Configuration install
 echo "Installing configuration ..."
 mkdir -p /etc/zabbix/zabbix_agentd.d/
-chown root:$ZABBIX_USER_NAME /etc/zabbix/zabbix_agentd.d/
+chown $ZABBIX_USER_NAME:$ZABBIX_USER_NAME /etc/zabbix/zabbix_agentd.d/
 
 # Log dir
 mkdir -p $ZABBIX_LOG_DIR_LOCATION
-chown root:$ZABBIX_USER_NAME $ZABBIX_LOG_DIR_LOCATION
+chown $ZABBIX_USER_NAME:$ZABBIX_USER_NAME $ZABBIX_LOG_DIR_LOCATION
 
 # Backup old zabbix configuration
 mv $ZABBIX_CONF_LOCATION "$ZABBIX_CONF_LOCATION".old
 touch $ZABBIX_CONF_LOCATION
-chown root:$ZABBIX_USER_NAME $ZABBIX_CONF_LOCATION
+chown $ZABBIX_USER_NAME:$ZABBIX_USER_NAME $ZABBIX_CONF_LOCATION
 
 # Install configuration
 echo "$ZABBIX_AGENT_CONFIGURATION" > $ZABBIX_CONF_LOCATION
@@ -131,12 +164,12 @@ if [ "$ZABBIX_PKS_ENABLED" -ne 0 ]; then
 	chmod 400 /etc/zabbix/zabbix_agentd.psk
 
 	ZABBIX_PKS_CONFIGUARION="# Zabbix PSK
-	TLSConnect=psk
-	TLSAccept=psk
-	TLSPSKFile=/etc/zabbix/zabbix_agentd.psk
-	TLSPSKIdentity=PSK-$SYSTEM_HOSTNAME
-	" 
-	echo $ZABBIX_PKS_CONFIGUARION >> $ZABBIX_CONF_LOCATION
+TLSConnect=psk
+TLSAccept=psk
+TLSPSKFile=/etc/zabbix/zabbix_agentd.psk
+TLSPSKIdentity=PSK-$SYSTEM_HOSTNAME
+" 
+	printf "%s\n" "$ZABBIX_PKS_CONFIGUARION" >> "$ZABBIX_CONF_LOCATION"
 fi
 
 # Restart Zabbix
@@ -146,11 +179,15 @@ systemctl restart zabbix-agent
 # Allow Zabbix in
 # UFW
 if command -v ufw >/dev/null; then
-	echo "Allow zabbix through firewall. UFW"
-	ufw allow from $ZABBIX_SERVER/32 to any port $ZABBIX_PORT comment "Allow zabbix-agent"
+	IFS=', '
+	for IP in $ZABBIX_SERVER; do
+		echo "Allow zabbix ($IP) through firewall. UFW"
+		ufw allow from $IP to any port $ZABBIX_PORT comment "Allow zabbix-agent"
+ 	done
 
 # FirewallD
 elif command -v firewall-cmd >/dev/null; then
+	# Yeah, this could be better.
 	echo "Allow zabbix through firewall. FirewallD"
 	firewall-cmd --zone=public --add-service=zabbix-agent --permanent
 	firewall-cmd --reload
@@ -163,5 +200,5 @@ if [ "$ZABBIX_PKS_ENABLED" -ne 0 ]; then
 	echo "Zabbix PSK ID: PSK-$SYSTEM_HOSTNAME"
 	echo "Zabbix PSK   : $ZABBIX_PSK"
 fi
-echo "The Zabbix agent has been installed and configured. Your primary IP is $SYSTEM_IP and the hostname is $SYSTEM_HOSTNAME"
+echo "The Zabbix agent has been installed and configured. Your primary IP is $SYSTEM_IP, $SYSTEM_IPV6 and the hostname is $SYSTEM_HOSTNAME"
 exit 0
